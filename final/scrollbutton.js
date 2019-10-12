7ede2fa3b2ee7616f4a65af5875e0f99e258c247
Jennifer Brady <twbird18@yahoo.com>
2019-10-12 21:59:10 +0000

    reformatted header, added anchors, changed containers

---
+++ final/scrollbutton.js
@@ -0,0 +1,20 @@
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 10px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
